144ab961e25660d9ddf9fbbb398147c02adb706c
Артём <56469391+ArtShp@users.noreply.github.com>
2021-06-25 17:11:40 +0300

    Some updates

--- main.py
+++ main.py
@@ -7,16 +7,38 @@ from datetime import datetime
 
 
 @eel.expose
-def download(url):
-    return url
+def download(url, path):
+    def progress_function(chunk, file_handle, bytes_remaining):
+        print(round((1 - bytes_remaining / vid.filesize) * 100, 3), '% done...')
 
+    try:
+        yt = YouTube(url, on_progress_callback=progress_function)
+    except VideoUnavailable:
+        print(f'Видео {url} недоступно, пропускаю')
+    except:
+        print(f'Ошибка для видео {url}, пропускаю')
+    else:
+        vid = yt.streams.filter(file_extension='mp4', progressive=True).get_highest_resolution()
+        #print(f'Видео {i+1} из {len(url)}')
+        #print('--------------------')
+        print(f'Ссылка: {url}')
+        for s in yt.streams:
+            print(s)
+        print(f'Автор: {yt.author}')
+        print(f'Название: {yt.title}')
+        print(f'Длительность: {yt.length}с')
+        print(f'Разрешение: {vid.resolution}')
+        print(f'Размер: {round(vid.filesize/(2**20))}MB')
+        print('--------------------')
+        print('Начало загрузки')
+        video_start = datetime.now()
+        vid.download(path)
+        print(f'Конец загрузки(Время: {datetime.now()-video_start})')
+        print('--------------------')
 
-def progress_function(chunk, file_handle, bytes_remaining):
-    print(round((1 - bytes_remaining / vid.filesize) * 100, 3), '% done...')
+    return 'Done!'
 
 """
-path = 'D:/download'
-
 urls = []
 
 data = open('yt.txt', 'r+', encoding='utf-8')
@@ -37,38 +59,16 @@ else:
     else:
         urls.append(n_data[0])
 
+
 print(f'Скачать: {len(urls)} видео')
 print('--------------------')
 program_start = datetime.now()
 
 for i in range(len(urls)):
-    try:
-        yt = YouTube(urls[i], on_progress_callback=progress_function)
-    except VideoUnavailable:
-        print(f'Видео {i+1}({urls[i]}) недоступно, пропускаю')
-    except:
-        print(f'Ошибка для видео {i+1}({urls[i]}), пропускаю')
-    else:
-        vid = yt.streams.filter(file_extension='mp4', progressive=True).get_highest_resolution()
-        #vid = yt.streams.order_by('resolution')[-1]
-        print(f'Видео {i+1} из {len(urls)}')
-        print('--------------------')
-        print(f'Ссылка: {urls[i]}')
-        for s in yt.streams:
-            print(s)
-        print(f'Автор: {yt.author}')
-        print(f'Название: {yt.title}')
-        print(f'Длительность: {yt.length}с')
-        print(f'Разрешение: {vid.resolution}')
-        print(f'Размер: {round(vid.filesize/(2**20))}MB')
-        print('--------------------')
-        print('Начало загрузки')
-        video_start = datetime.now()
-        vid.download(path)
-        print(f'Конец загрузки(Время: {datetime.now()-video_start})')
-        print('--------------------')
+    
 
 print(f'Конец работы(Время: {datetime.now()-program_start})')
+
 """
 
 eel.init('web')
